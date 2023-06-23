2b6ec815ee824108b60a58b54b270c29b098afb9
Patrick Pichler <ppichler@outlook.at>
2023-06-23 23:05:02 +0200

    Solves Day 6 of 2022

---
+++ src/main/kotlin/year2022/day-06.kt
@@ -0,0 +1,52 @@
+package year2022
+
+import lib.Day
+import lib.Part
+import lib.TestCase
+
+fun main() {
+    Day(6, 2022, PartA6(4), PartB6()).run()
+}
+
+
+open class PartA6(private var packetLength: Int) : Part() {
+    internal lateinit var text: String
+    override fun parse(text: String) {
+        this.text = text
+    }
+
+    override fun compute(): String {
+        (packetLength until text.length).forEach {
+            if (allDifferent(text.slice(it-packetLength until it))) {
+                return it.toString()
+            }
+        }
+        return "Packet not found"
+    }
+
+    private fun allDifferent(text: String): Boolean {
+        return text.toSet().size == text.length
+    }
+
+    override val exampleAnswer: String
+        get() = "7"
+
+    override val testCases = sequence {
+        yield(TestCase("Example2", "bvwbjplbgvbhsrlpgdmjqwftvncz", "5"))
+        yield(TestCase("Example3", "nppdvjthqldpwncqszvftbrmjlhg", "6"))
+        yield(TestCase("Example4", "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "10"))
+        yield(TestCase("Example5", "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "11"))
+    }
+}
+
+class PartB6 : PartA6(14) {
+    override val exampleAnswer: String
+        get() = "19"
+
+    override val testCases = sequence {
+        yield(TestCase("Example2", "bvwbjplbgvbhsrlpgdmjqwftvncz", "23"))
+        yield(TestCase("Example3", "nppdvjthqldpwncqszvftbrmjlhg", "23"))
+        yield(TestCase("Example4", "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "29"))
+        yield(TestCase("Example5", "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "26"))
+    }
+}
