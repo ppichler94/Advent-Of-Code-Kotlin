fff75a2fbf6cf63e1d0846fe1909b024a45179d1
Patrick Pichler <ppichler@outlook.at>
2023-06-28 21:46:02 +0200

    use multik insted of vector2i

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,7 @@ kotlin {
 }
 
 dependencies {
-    implementation("org.jsoup:jsoup:1.14.3")
+    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("org.jetbrains.kotlinx:multik-core:0.2.2")
+    implementation("org.jetbrains.kotlinx:multik-default:0.2.2")
 }
--- src/main/kotlin/year2022/day-08.kt
+++ src/main/kotlin/year2022/day-08.kt
@@ -2,8 +2,12 @@ package year2022
 
 import lib.Day
 import lib.Part
-import lib.math.Vector2i
 import lib.math.product
+import org.jetbrains.kotlinx.multik.api.mk
+import org.jetbrains.kotlinx.multik.api.ndarrayOf
+import org.jetbrains.kotlinx.multik.ndarray.data.D1Array
+import org.jetbrains.kotlinx.multik.ndarray.data.get
+import org.jetbrains.kotlinx.multik.ndarray.operations.plus
 
 fun main() {
     Day(8, 2022, PartA8(), PartB8()).run()
@@ -21,24 +25,24 @@ open class PartA8 : Part() {
     }
 
     override fun compute(): String {
-        val visible = mutableSetOf<Vector2i>()
+        val visible = mutableSetOf<D1Array<Int>>()
         limits.forEach {
-            visible.addAll(walkLine(Vector2i(0, it), Vector2i(1, 0)))
-            visible.addAll(walkLine(Vector2i(trees.size - 1, it), Vector2i(-1, 0)))
-            visible.addAll(walkLine(Vector2i(it, 0), Vector2i(0, 1)))
-            visible.addAll(walkLine(Vector2i(it, trees.size - 1), Vector2i(0, -1)))
+            visible.addAll(walkLine(mk.ndarrayOf(0, it), mk.ndarrayOf(1, 0)))
+            visible.addAll(walkLine(mk.ndarrayOf(trees.size - 1, it), mk.ndarrayOf(-1, 0)))
+            visible.addAll(walkLine(mk.ndarrayOf(it, 0), mk.ndarrayOf(0, 1)))
+            visible.addAll(walkLine(mk.ndarrayOf(it, trees.size - 1), mk.ndarrayOf(0, -1)))
         }
         return visible.size.toString()
     }
 
-    private fun walkLine(startPos: Vector2i, direction: Vector2i): MutableSet<Vector2i> {
-        val currentPos = startPos.toMutableVector()
+    private fun walkLine(startPos: D1Array<Int>, direction: D1Array<Int>): MutableSet<D1Array<Int>> {
+        var currentPos = startPos
         var currentHeight = -1
-        val visible = mutableSetOf<Vector2i>()
-        while (currentPos within limits) {
-            if (trees[currentPos.y][currentPos.x] > currentHeight) {
-                visible.add(currentPos.toVector())
-                currentHeight = trees[currentPos.y][currentPos.x]
+        val visible = mutableSetOf<D1Array<Int>>()
+        while (currentPos[0] in limits && currentPos[1] in limits) {
+            if (trees[currentPos[1]][currentPos[0]] > currentHeight) {
+                visible.add(currentPos)
+                currentHeight = trees[currentPos[1]][currentPos[0]]
             }
             currentPos += direction
         }
@@ -58,17 +62,17 @@ class PartB8 : PartA8() {
     }
 
     private fun calculateScenicScore(value: Pair<Int, Int>): Int {
-        val position = Vector2i(value.first, value.second)
-        val directions = listOf(Vector2i(-1, 0), Vector2i(1, 0), Vector2i(0, -1), Vector2i(0, 1))
+        val position = mk.ndarrayOf(value.first, value.second)
+        val directions = listOf(mk.ndarrayOf(-1, 0), mk.ndarrayOf(1, 0), mk.ndarrayOf(0, -1), mk.ndarrayOf(0, 1))
         return directions.map { calculateScoreOfLine(position, it) }.reduce(Int::times)
     }
 
-    private fun calculateScoreOfLine(position: Vector2i, direction: Vector2i): Int {
-        val currentPos = (position + direction).toMutableVector()
+    private fun calculateScoreOfLine(position: D1Array<Int>, direction: D1Array<Int>): Int {
+        var currentPos = position + direction
         var score = 0
-        while (currentPos within limits) {
+        while (currentPos[0] in limits && currentPos[1] in limits) {
             score += 1
-            if (trees[currentPos.y][currentPos.x] >= trees[position.y][position.x]) {
+            if (trees[currentPos[1]][currentPos[0]] >= trees[position[1]][position[0]]) {
                 break
             }
             currentPos += direction
