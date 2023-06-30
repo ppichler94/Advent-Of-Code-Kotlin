f90a8a411ecc0e29a8aaa43238e55d82c07677de
Patrick Pichler <ppichler@outlook.at>
2023-06-30 13:43:52 +0200

    Revert "use multik insted of vector2i"
    This reverts commit fff75a2fbf6cf63e1d0846fe1909b024a45179d1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,5 @@ kotlin {
 }
 
 dependencies {
-    implementation("org.jsoup:jsoup:1.15.3")
-    implementation("org.jetbrains.kotlinx:multik-core:0.2.2")
-    implementation("org.jetbrains.kotlinx:multik-default:0.2.2")
+    implementation("org.jsoup:jsoup:1.14.3")
 }
