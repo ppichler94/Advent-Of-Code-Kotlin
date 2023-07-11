dea6555686fb0eefb08ff0c836b31a3c94df566d
Patrick Pichler <ppichler@outlook.at>
2023-07-11 23:47:50 +0200

    uses measureTimedValue instead of measureTimeMillis

--- src/main/kotlin/lib/Part.kt
+++ src/main/kotlin/lib/Part.kt
@@ -1,6 +1,7 @@
 package lib
 
-import kotlin.system.measureTimeMillis
+import kotlin.time.DurationUnit
+import kotlin.time.measureTimedValue
 
 enum class PartName(val value: Int) {
     A(1), B(2)
@@ -8,7 +9,7 @@ enum class PartName(val value: Int) {
 
 data class TestCase(val name: String, val input: String, val expected: String)
 
-abstract class Part() {
+abstract class Part {
     abstract fun parse(text: String)
     abstract fun compute(): String
 
@@ -31,10 +32,8 @@ abstract class Part() {
     private fun runTest(testCase: TestCase): Boolean {
         print("  ")
         parse(testCase.input)
-        var solution: String
-        val runtime = measureTimeMillis {
-            solution = compute()
-        }
+        val timedValue = measureTimedValue { compute() }
+        val (solution, runtime) = timedValue
         val successful = testCase.expected.isEmpty() || solution == testCase.expected
         print(Util.colored(">> ", if (successful) Util.Color.GREEN else Util.Color.RED))
         print("TestCase '${testCase.name}' ")
@@ -44,20 +43,16 @@ abstract class Part() {
             print(Util.colored("failed", Util.Color.RED))
             print(" Expected: ${testCase.expected}")
         }
-        val runtimeString = "%.2f".format(runtime.toDouble() / 1000)
-        print(" Result: $solution Runtime: ${runtimeString}s\n")
+        print(" Result: $solution Runtime: ${runtime.toString(DurationUnit.SECONDS, 3)}\n")
         return successful
     }
 
     private fun runPuzzle(inputData: String): String {
         println("Running puzzle...")
         parse(inputData)
-        var solution: String
-        val runtime = measureTimeMillis {
-            solution = compute()
-        }
-        val runtimeString = "%.2f".format(runtime.toDouble() / 1000)
-        println("  >> Runtime: ${runtimeString}s")
+        val timedValue = measureTimedValue { compute() }
+        val (solution, runtime) = timedValue
+        println("  >> Runtime: ${runtime.toString(DurationUnit.SECONDS, 3)}")
         return solution
     }
 
