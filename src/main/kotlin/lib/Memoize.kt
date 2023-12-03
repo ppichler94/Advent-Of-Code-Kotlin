0c14c394a61a7d804e410ac78aeebc2a6b6d51c4
Patrick Pichler <ppichler@outlook.at>
2023-12-03 23:50:18 +0100

    extends memoize

--- src/main/kotlin/lib/Memoize.kt
+++ src/main/kotlin/lib/Memoize.kt
@@ -1,5 +1,7 @@
 package lib
 
+import java.io.Serializable
+
 /**
  * Memoization of function results. Can only be used with pure functions (i.e. the return value only depends on the
  * arguments). The arguments of the function to be memoized must be hashable (equals and hashCode is implemented).
@@ -14,4 +16,16 @@ fun <A, B, R> ((A, B) -> R).memoize(initialCapacity: Int = 256): (A, B) -> R = m
 
 fun <A, B, R> ((A, B) -> R).memoize(cache: MutableMap<Pair<A, B>, R>): (A, B) -> R = { a: A, b: B ->
     cache.getOrPut(a to b) { this(a, b) }
+}
+
+fun <A, B, C, D, R> ((A, B, C, D) -> R).memoize(initialCapacity: Int = 256): (A, B, C, D) -> R =
+    memoize(HashMap(initialCapacity))
+
+fun <A, B, C, D, R> ((A, B, C, D) -> R).memoize(cache: MutableMap<Tuple4<A, B, C, D>, R>): (A, B, C, D) -> R =
+    { a: A, b: B, c: C, d: D ->
+        cache.getOrPut(Tuple4(a, b, c, d)) { this(a, b, c, d) }
+    }
+
+data class Tuple4<A, B, C, D>(val a: A, val b: B, val c: C, val d: D) : Serializable {
+    override fun toString(): String = "($a, $b, $c, $d)"
 }
