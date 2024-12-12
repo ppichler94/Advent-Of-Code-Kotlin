268e13f4b2a423980a922621a1eb510bffd7c2ed
Patrick Pichler <ppichler@outlook.at>
2024-12-12 23:39:06 +0100

    feat: solves day 1 of 2024

---
+++ src/main/kotlin/year2024/day-01.kt
@@ -0,0 +1,37 @@
+package year2024
+
+import lib.aoc.Day
+import lib.aoc.Part
+import kotlin.math.abs
+
+fun main() {
+    Day(1, 2024, PartA1(), PartB1()).run()
+}
+
+open class PartA1 : Part() {
+    protected lateinit var left: List<Int>
+    protected lateinit var right: List<Int>
+
+    override fun parse(text: String) {
+        left = text.lines().map { it.split(" ").first().toInt() }.sorted()
+        right = text.lines().map { it.split(" ").last().toInt() }.sorted()
+    }
+
+    override fun compute(): String {
+        return left.zip(right).sumOf { (a, b) -> abs(a - b)}.toString()
+    }
+
+    override val exampleAnswer: String
+        get() = "11"
+}
+
+class PartB1 : PartA1() {
+
+    override fun compute(): String {
+        val occurances = right.groupingBy { it }.eachCount()
+        return left.sumOf { (occurances[it] ?: 0) * it }.toString()
+    }
+
+    override val exampleAnswer: String
+        get() = "31"
+}
